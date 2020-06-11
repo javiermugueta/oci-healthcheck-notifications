6a0ddf886f307311ed40cf3c0043ec1d310f4444
javier mugueta <javiermugueta@mbpj.local>
2020-06-12 00:45:03 +0200

    .

--- ohcm.sh
+++ ohcm.sh
@@ -8,11 +8,11 @@ MONITORDESCRIPTION=$4
 MESSAGE="OCI healthchechs monitor. There are healthchecks in red, that is, failed for $MONITORDESCRIPTION!!!"
 QUERY='data[?"status-code"!="200"]."error-message"'
 DATE=`date`
-COMMAND="oci health-checks http-probe-result list --probe-configuration-id $PROBEID --query '$QUERY' --limit $LIMIT --sort-order DESC"
-echo $COMMAND
-RESULT=$COMMAND
-echo $RESULT
-if [[ $RESULT != '' ]]; then
+COMMAND="oci health-checks http-probe-result list --probe-configuration-id $PROBEID --query $QUERY --limit $LIMIT --sort-order DESC"
+echo "$COMMAND"
+RESULT=$($COMMAND)
+echo "$RESULT"
+if [[ "$RESULT" != '' ]]; then
     echo "KO"
     oci ons message publish --topic-id $2 --body "{date: $DATE, msg: $MESSAGE}"
 else
