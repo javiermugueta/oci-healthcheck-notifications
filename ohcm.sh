0a128411c9b6f2d8f9d4117a0c265a9bea2b32e8
javier mugueta <javiermugueta@mbpj.local>
2020-06-11 23:46:03 +0200

    .

--- ohcm.sh
+++ ohcm.sh
@@ -3,7 +3,7 @@
 PROBEID=$1
 TOPIC=$2
 LIMIT=$3
-MESSAGE="OCI healthchechs monitor, there are healthchecks in red, that is failed"
+MESSAGE="OCI healthchechs monitor, there are healthchecks in red, that is, failed!!!"
 QUERY='data[?"is-healthy"!=`true`]."probe-configuration-id"'
 DATE=`date`
 COMMAND="oci health-checks http-probe-result list --probe-configuration-id $PROBEID --query $QUERY --limit $LIMIT"
